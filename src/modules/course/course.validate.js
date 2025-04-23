e04108eb7c64157b4d8b32918332d61fd347c3bd
Riham <moriham190@gmail.com>
2025-04-23 14:37:19 +0200

    ch

--- src/modules/course/course.validate.js
+++ src/modules/course/course.validate.js
@@ -4,20 +4,7 @@ import Joi from 'joi';
 export const addCourseSchema = Joi.object({
   title: Joi.string().trim().required(),
   description: Joi.string().trim().min(10).required(),
-  image: Joi.object({
-  fieldname: Joi.string().required(),       
-  originalname: Joi.string().required(),    
-  encoding: Joi.string().required(),        
-  mimetype: Joi.string()
-    .valid('image/jpeg', 'image/png', 'image/gif', 'image/jpg')
-    .required(),  
-  size: Joi.number()
-    .max(5242880)  
-    .required(),  
-   destination: Joi.string().required(),  
-  filename: Joi.string().required(),     
-  path: Joi.string().required()          
-}),
+  image: Joi.string(),
   startDate: Joi.date().optional(),
   endDate: Joi.date().optional(),
   price: Joi.number().required(),
