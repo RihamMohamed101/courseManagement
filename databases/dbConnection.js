2e9e0e477796d49cf2019a12e811a3d153dbe4a4
Riham <moriham190@gmail.com>
2025-04-23 14:14:20 +0200

    dbChanges

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 
-export const dbConnection = mongoose.connect("mongodb://localhost:27017/courseMangement").then(() => {
+export const dbConnection = mongoose.connect("mongodb+srv://Riham:0ahCUTj7HjinJ8wI@cluster0.rs75a.mongodb.net/courseMangement").then(() => {
       console.log("databaeConnect");
 })
